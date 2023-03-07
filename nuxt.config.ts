a444fbd2fd3c8a4d658ece8f794a49bb7e012746
qader qaribiyan <gharibyan.ghader@gmail.com>
2023-03-07 16:21:23 +0100

    add pico css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  app: {
+    head: {
+      meta: [
+        // <meta name="viewport" content="width=device-width, initial-scale=1">
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      script: [
+        // <script src="https://myawesome-lib.js"></script>
+        { src: "https://awesome-lib.js" },
+      ],
+      link: [
+        // <link rel="stylesheet" href="https://myawesome-lib.css">
+        { rel: "stylesheet", href: "https://unpkg.com/@picocss/pico@1.*/css/pico.min.css" },
+      ],
+      // please note that this is an area that is likely to change
+      style: [
+        // <style type="text/css">:root { color: red }</style>
+        //{ children: ":root { color: red }", type: "text/css" },
+      ],
+      noscript: [
+        // <noscript>JavaScript is required</noscript>
+        { children: "JavaScript is required" },
+      ],
+    },
+  },
+});
